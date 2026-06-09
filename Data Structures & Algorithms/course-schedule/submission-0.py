6228a5ee8037fc5956d8f1a3cc02266c4a7eb66f
olruss <oorlov139@gmail.com>
2026-06-09 14:23:41 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # cycle  found - can't complete
+        # course is complete when no prerequisites left
+
+        adj = {i: [] for i in range(numCourses)}
+        for c, p in prerequisites:
+            adj[c].append(p)
+            
+        completed = set()
+        seen = set()
+
+        def dfs(i):
+            if i in seen:
+                return False
+            if i in completed:
+                return True
+            prerequisites = adj[i]
+            if len(prerequisites) == 0:
+                completed.add(i)
+                return True
+            
+            seen.add(i)
+            for preq in prerequisites:
+                res = dfs(preq)
+                if not res:
+                    return False
+            seen.remove(i)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True
